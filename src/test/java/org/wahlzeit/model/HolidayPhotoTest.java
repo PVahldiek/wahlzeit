5f5aea4b3b8a78f91fc67c6c57d5367639bdd263
Pascal Vahldiek <pascal.vahldiek@fau.de>
2020-11-21 03:45:18 +0100

    fixed test for pipeline run

--- src/test/java/org/wahlzeit/model/HolidayPhotoTest.java
+++ src/test/java/org/wahlzeit/model/HolidayPhotoTest.java
@@ -4,5 +4,12 @@ import junit.framework.TestCase;
 
 public class HolidayPhotoTest extends TestCase {
 
-
+    public void testHasHolidays(){
+        Holiday holiday = new Holiday(10, 2000, "Australia");
+        HolidayPhoto holidayPhoto = new HolidayPhoto();
+        holidayPhoto.setHoliday(holiday);
+        assertEquals(holidayPhoto.getHoliday().getDays(), 10);
+        assertEquals(holidayPhoto.getHoliday().getCosts(), 2000);
+        assertEquals(holidayPhoto.getHoliday().getCountry(), "Australia");
+    }
 }
