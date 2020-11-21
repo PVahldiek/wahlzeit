d886591983d07cb12eea42afa442d0e430e67a0e
Pascal Vahldiek <pascal.vahldiek@fau.de>
2020-11-21 03:36:50 +0100

    test class with empty tests for coordinate

---
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -0,0 +1,18 @@
+package org.wahlzeit.model;
+
+import junit.framework.TestCase;
+
+public class CoordinateTest extends TestCase {
+
+    public void testGetDistance() {
+    }
+
+    public void testIsEqual() {
+    }
+
+    public void testTestEquals() {
+    }
+
+    public void testTestHashCode() {
+    }
+}
