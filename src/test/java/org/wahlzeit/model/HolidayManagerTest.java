aadd6522aa82b8b07784070fa9efd58a958c5630
Pascal Vahldiek <pascal.vahldiek@fau.de>
2021-01-25 04:35:16 +0100

    test for createHoliday

--- src/test/java/org/wahlzeit/model/HolidayManagerTest.java
+++ src/test/java/org/wahlzeit/model/HolidayManagerTest.java
@@ -1,4 +1,16 @@
 package org.wahlzeit.model;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class HolidayManagerTest {
+
+    @Test
+    public void testCreateHoliday() {
+
+        HolidayManager holidayManager = new HolidayManager();
+        HolidayType holidayType = new HolidayType("IceLandType");
+        Holiday holiday = holidayManager.createHoliday("IceLandType");
+        Assert.assertTrue(!holidayManager.getHolidays().isEmpty());
+    }
 }
